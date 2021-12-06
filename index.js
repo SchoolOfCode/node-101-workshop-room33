98ab8007e37855ecea0ed59ce9d105603fbb065c
Kevwe Olomu <kevweolomu@yahoo.ca>
2021-12-06 23:01:39 +0000

    Task completed

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-let myCollection = [
-  {
-    name: "School of Code mug",
-    count: 1,
-    whatILike: "It has my cute pixel character on it!",
-  },
-  {
-    name: "School of Code hat",
-    count: 2,
-    whatILike: "An often overlooked fashion accessory",
-  },
-  {
-    name: "School of Code pillow",
-    count: 1,
-    whatILike: "Eat. Sleep. Code. Repeat :)",
-  },
-];
+// 2b, 3c
+import chalk from "chalk";
+console.log(chalk.blue('Hello world!'));
 
+import myCollection from "./collection.js";
 console.log(myCollection);
-// If there's only one of the item in your collection, it should log "I have a name. Here's what I like about it: whatILike". If you have more than one of it, the message should log "I have count names. Here's what I like about them: whatILike".
 
-function describeItem(myCollection){
-  for (let i = 0; i < myCollection.length; i++){
-      if(myCollection[i].count > 1){
-        console.log(`I have ${myCollection[i].count, myCollection[i].name}s. Here's what I like about it: ${myCollection[i].whatILike}!`);
+//2c 
+function describeItem(item){
+    if (item.count === 1) {
+        console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`)
     } else {
-      console.log(`I have a ${myCollection[i].name}. Here's what I like about it: ${myCollection[i].whatILike}!`);
+        console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`)
     }
-
-  }
-        
- 
 }
 
-describeItem(myCollection);
+describeItem(myCollection[0]);
+
+// 2d
+function describeCollection (array){
+  array.forEach(describeItem);
+}
+
+describeCollection(myCollection);
